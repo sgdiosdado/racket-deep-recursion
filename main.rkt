b9c29e7e4321a2111ccf37f3a355a5ba473c8291
Iñaki Janeiro Olague <inaki_janeiro@live.com.mx>
2020-09-11 22:42:30 -0500

    Add missing problems and fix palindromo

--- main.rkt
+++ main.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-; menor: returns the smallest integer in a list
+; Function menor: returns the smallest integer in a list
 (define (menor l)
   (if (= (length l) 1)
       (car l)
@@ -10,19 +10,22 @@
       )
   )
 
-; palindromo: returns the palindrome from 0...N
-(define (appendAndShift l n)
-  (append (reverse (append l (cons n '()))) (cons n '()))
-  )
-
-(define (palindromo n)
+; Function palindromo: returns the palindrome from 0...N
+(define (palindromo-aux-two n)
   (if (= n 0)
-      (appendAndShift '() 0)
-      (appendAndShift (palindromo (- n 1)) n)
-      )
+      (cons 0 '())
+      (cons n (palindromo-aux-two (- n 1)))))
+
+(define (palindromo-aux n c)
+  (if (= n c)
+      (cons n (palindromo-aux-two n))
+      (cons c (palindromo-aux n (+ c 1)))))
+                   
+(define (palindromo n)
+  (palindromo-aux n 0)
   )
 
-; negativos: returns a list of only negative numbers of a given list
+; Function negativos: returns a list of only negative numbers of a given list
 (define (negativos l)
   (if (empty? l)
       '()
@@ -33,7 +36,7 @@
       )
   )
 
-;profundidad: returns the depth of a possible nested list
+; Function profundidad: returns the depth of a possible nested list
 
 (define (profundidad l)
   (cond ((null? l) 0)
@@ -43,7 +46,23 @@
         
   )
 
+; Function simetrico: returns a balanced sequence of N "(<()>) wheren N=0: () and N=1 (< () >) and so on...
+(define (simetrico n)
+  (if (= n 0)
+      '()
+      (list '< (simetrico (- n 1)) '>)))
 
+; Function elimina: removes all appereances of element "dato" in a possible nested list
+(define (elimina dato lista)
+  (cond [(null? lista) '()]
+        [(list? (car lista)) (cons (elimina dato (car lista)) (elimina dato (cdr lista)))]
+        [(equal? dato (car lista)) [if (null? (cdr lista))
+                                   '()
+                                   (elimina dato (cdr lista))
+                                   ]]
+        [else (cons (car lista) (elimina dato (cdr lista)))]
+  )
+)
 
 
 
